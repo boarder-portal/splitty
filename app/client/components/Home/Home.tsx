55d28d58523fe64c5149401259083c264d7e5e87
atooxa <attooxa@yandex.ru>
2020-02-21 01:46:15 +0500

    Add eslint react hooks rule.

--- app/client/components/Home/Home.tsx
+++ app/client/components/Home/Home.tsx
@@ -1,13 +1,28 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const Home: React.FC = () => {
+  const [roomName, setRoomName] = useState('');
+
+  const handleRoomNameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomName(e.target.value);
+  }, []);
+
   const handleCreateRoomClick = useCallback(() => {
     console.log('Add room');
   }, []);
 
   return (
     <div>
-      <div>Splity</div>
+      <h1>Splity</h1>
+
+      <div>
+        <div>Введите название комнаты (поезда, кафе)</div>
+
+        <input
+          value={roomName}
+          onChange={handleRoomNameChange}
+        />
+      </div>
 
       <button onClick={handleCreateRoomClick}>Создать комнату</button>
     </div>
