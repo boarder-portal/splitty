abb58c2c1d18d69e69abbfc09ae94be5b8c99a29
atooxa <attooxa@yandex.ru>
2024-01-11 02:13:59 +0500

    Prisma create user and reads users

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('users')
+  async getUsers() {
+    return prisma.user.findMany();
+  }
+
+  @Post('users')
+  createUser(@Body('id') id: number) {
+    return prisma.user.create({
+      data: {
+        id,
+      },
+    });
   }
 }
